fa17eb3f039244ef515962c56c2bc6d0d2821d1f
KauanJesusJD <kauan.santos76@etec.sp.gov.br>
2023-11-01 12:12:40 -0300

    Tentando adicionar as linhas

--- js/script.js
+++ js/script.js
@@ -4,54 +4,52 @@ const ctx = canvas.getContext('2d');//Linkando o canvas com o HTML
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = 700;
 
+const palavras = ['MINECRAFT', 'PICA-PAU'];
+var sortear = palavras[Math.floor(Math.random() * 2)];
+var quantLetras = sortear.length;
 
-const numA = Math.floor(Math.random() * 1) + 1 ;
+const numA = Math.floor(Math.random() * 10) + 1;
 console.log(numA);
 
 if (numA == 1) {
     ctx.font = "25px arial";
-    ctx.fillText("Dica:Um dos jogos mais famosos e vendidos pelo mundo", 10, 50);//Minecraft
-    
-    ctx.beginPath();
-    ctx.moveTo(930,130);
-    ctx.lineTo(1030,130);
-    ctx.stroke();
+    ctx.fillText("Dica: Um dos jogos mais famosos e vendidos pelo mundo", 10, 680);//Minecraft
 
 } else if (numA == 2) {
     ctx.font = "25px arial";
-    ctx.fillText("Dica:Especíe de animal extinta ", 10, 50);//Pica pau
+    ctx.fillText("Dica: Especíe de animal extinta ", 10, 680);//Pica pau
 
-} else if(numA == 3) {
+} else if (numA == 3) {
     ctx.font = "25px arial";
 
-    ctx.fillText("Dica:Profissão", 10, 50);//Psicologo
-} else if(numA == 4) {
+    ctx.fillText("Dica: Profissão", 10, 680);//Psicologo
+} else if (numA == 4) {
     ctx.font = "25px arial";
-    ctx.fillText("Dica:Comida", 10, 50);//Calabresa
+    ctx.fillText("Dica: Comida", 10, 680);//Calabresa
 
-} else if(numA == 5) {
-    ctx.font ="25px arial"
-    ctx.fillText("Dica:Linguagem de programação", 10, 50);//Javascript
+} else if (numA == 5) {
+    ctx.font = "25px arial"
+    ctx.fillText("Dica: Linguagem de programação", 10, 680);//Javascript
 
-} else if(numA == 6) {
-    ctx.font ="25px arial"
-    ctx.fillText("Dica:Parte do corpo humano", 10, 50);//Coração
+} else if (numA == 6) {
+    ctx.font = "25px arial"
+    ctx.fillText("Dica: Parte do corpo humano", 10, 680);//Coração
 }
- else if(numA == 7) {
-    ctx.font ="25px arial"
-    ctx.fillText("Dica:Fruta", 10, 50);//Abacaxi
+else if (numA == 7) {
+    ctx.font = "25px arial"
+    ctx.fillText("Dica: Fruta", 10, 680);//Abacaxi
 
-} else if(numA == 8) {
-    ctx.font ="25px arial"
-    ctx.fillText("Dica:Entrada de energia", 10, 50);//Tomada
+} else if (numA == 8) {
+    ctx.font = "25px arial"
+    ctx.fillText("Dica: Entrada de energia", 10, 680);//Tomada
 
-} else if(numA == 9) {
-    ctx.font ="25px arial"
-    ctx.fillText("Dica:Animal", 10, 50);//Cachcorro
+} else if (numA == 9) {
+    ctx.font = "25px arial"
+    ctx.fillText("Dica: Animal", 10, 680);//Cachcorro
 
-} else if(numA == 10) {
-    ctx.font ="25px arial"
-    ctx.fillText("Dica:Cor", 10, 50);//Vermelho
+} else if (numA == 10) {
+    ctx.font = "25px arial"
+    ctx.fillText("Dica: Cor", 10, 680);//Vermelho
 
 }
 
@@ -60,6 +58,6 @@ if (numA == 1) {
 
 
 
-    
+
 
 
