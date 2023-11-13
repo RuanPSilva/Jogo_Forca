8cb92de6eea41bf3206ab141d61dc98a35c02688
KauanJesusJD <kauan.santos76@etec.sp.gov.br>
2023-11-12 22:18:21 -0300

    Terminando o codigo, mas o jogo nao funciona direito

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');//Linkando o canvas com o HTML
 
-ctx.canvas.width = window.innerWidth;
-ctx.canvas.height = 700;
+ctx.canvas.width = 700;
+ctx.canvas.height = 300;
 
 const palavras = ['MINECRAFT', 'PICA-PAU', 'PSICOLOGO', 'CALABRESA', 'JAVASCRIPT', 'CORAÇÃO', 'ABACAXI', 'TOMADA', 'CACHORRO', 'VERMELHO'];
-var sortear = palavras[Math.floor(Math.random() * 2)];
-var erros = 0;
+var quantidadeErros = 0;
 var acertos = 0;
 var tentativas = "";
+palavraSecreta  = palavras[Math.floor(Math.random() * 10)];
 
 desenharPoste();
 desenharBarraSuperior();
 desenharApoio();
 desenharTracos();
 
-window.onkeypress = teclaApertada;
+window.onkeypress = teclaPressionada;
 
-function teclaApertada() {
-    if (!tentativas.includes(event.key) && sortear.includes((event.key).toUpperCase())) {
+function teclaPressionada() {
+    if (!tentativas.includes(event.key) && palavraSecreta.includes((event.key).toUpperCase())) {
         adicionarTentativa();
-        for (var i = 0; i < sortear.length; i++) {
-            if (sortear[i] == (event.key).toUpperCase()) {
+        for (var i = 0; i < palavraSecreta.length; i++) {
+            if (palavraSecreta [i] == (event.key).toUpperCase()) {
                 ctx.fonte = "35px";
                 ctx.fillText((event.key).toUpperCase(), 20 + (35 * 1), 200);
                 acertos++;
@@ -29,8 +29,8 @@ function teclaApertada() {
         }
     } else {
         adicionarTentativa();
-        erros;
-        desenharBoneco(erros);
+        quantidadeErros;
+        desenharBoneco(quantidadeErros);
     }
     verificarFimJogo();
 }
@@ -44,12 +44,109 @@ function adicionarTentativa() {
 }
 
 function verificarFimJogo() {
-    if (erros >= 6) {
+    if (quantidadeErros >= 6) {
         ctx.font = "20px Arial";
         ctx.fillText("Game Over!")
 
-    }
+ 
+   }
+   if (acertos == palavraSecreta.length) {
+	   ctx.font = "20px Arial";
+	   ctx.fillText("Voçê ganhou", 200, 100);
+	   window.onkeypress= null;
+	   return;
+   }
+   
 }
+
+function desenharPoste() {
+	ctx.moveTo(10, 10);
+	ctx.lineTo(10, 100);
+	ctx.stroke();
+}
+
+function desenharBarraSuperior() {
+	ctx.moveTo(10, 10);
+	ctx.lineTo(60, 30);
+	ctx.stroke();
+}
+
+function desenharApoio() {
+	ctx.moveTo(60, 10);
+	ctx.lineTo(60, 30);
+	ctx.stroke();
+}
+
+function desenharTracos() {
+	for (var i = 0; i < palavraSecreta.length; i++) {
+		ctx.moveTo(20 + (35 * i), 200);
+		ctx.lineTo(50 + (35 * i), 200);
+		ctx.stroke();
+	}
+}
+
+function desenharBoneco(quantidadeErros) {
+	switch (quantidadeErros) {
+		case 1:
+			desenharCabeca();
+			break;
+		case 2:
+			desenharTronco();
+			break;
+		case 3:
+			desenharBracoEsquerdo();
+			break;
+		case 4:
+			desenharBracoDireito();
+			break;
+		case 5:
+			desenharPernaEsquerda();
+			break;
+		case 6:
+			desenharPernaDireita();
+			break;
+	}
+}
+
+function desenharCabeca() {
+	ctx.beginPath();
+	ctx.arc(50, 40, 10, 0, 2 * Math.PI);
+	ctx.stroke();
+}
+
+function desenharTronco() {
+	ctx.moveTo(60, 50);
+	ctx.lineTo(60, 80);
+	ctx.stroke();
+}
+
+function desenharBracoEsquerdo() {
+	ctx.moveTo(60, 60);
+	ctx.lineTo(50, 70);
+	ctx.stroke();
+}
+
+function desenharBracoDireito() {
+	ctx.moveTo(60, 60);
+	ctx.lineTo(70, 70);
+	ctx.stroke();
+}
+
+function desenharPernaEsquerda() {
+	ctx.moveTo(60, 80);
+	ctx.lineTo(50, 90);
+	ctx.stroke();
+}
+
+function desenharPernaDireita() {
+	ctx.moveTo(60, 80);
+	ctx.lineTo(70, 90);
+	ctx.stroke();
+}
+
+		
+	
+
 
 
 
